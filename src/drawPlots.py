e359d28a0fddc679455d1701ea95080e4207fa5e
Luis <lerhardt@rhrk.uni-kl.de>
2023-09-27 16:26:23 +0200

    epochs param is now array

--- src/drawPlots.py
+++ src/drawPlots.py
@@ -1,14 +1,11 @@
-import json
-import torch
 from matplotlib import pyplot
 from pathlib import Path
 
-
-def plot_D_probabilities(num_epochs, probs_test, probs_train):
+def plot_D_probabilities(epochs, probs_test, probs_train):
     print("Plot probabilitiy outputs of discriminator")
     fig, ax = pyplot.subplots()
-    ax.plot(range(num_epochs), probs_test, 'r', label='test samples')
-    ax.plot(range(num_epochs), probs_train, 'b', label='train samples')
+    ax.plot(epochs, probs_test, 'r', label='test samples')
+    ax.plot(epochs, probs_train, 'b', label='train samples')
     ax.legend()
     ax.set_xlabel('Epoch')
     ax.set_ylabel('Probability output of D')
@@ -16,6 +13,3 @@ def plot_D_probabilities(num_epochs, probs_test, probs_train):
     Path("plots").mkdir(parents=False, exist_ok=True)
     pyplot.savefig(filename)
     pyplot.close()
-
-probs = json.load(open("probs.json" ))
-plot_D_probabilities(probs["epochs"], probs["test_probs"], probs["train_probs"])
